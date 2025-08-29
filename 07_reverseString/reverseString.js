04e9fc4983737612301c508f8ee79135bb87cc8c
Akrame-Amine Dridi <aadridi@gmail.com>
2025-08-30 00:35:18 +0200

    Complete exercise n°7 as per the instructions

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	const stringSplit = string.split('');
+	const stringReverse = stringSplit.reverse();
+	const stringJoined = stringReverse.join('');
+	return stringJoined;
 };
 
+console.log(reverseString('hello there'));
 // Do not edit below this line
 module.exports = reverseString;
