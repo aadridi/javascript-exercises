2058757c59a7075c8c748a57d785d356be29e7f2
Akrame-Amine Dridi <aadridi@gmail.com>
2025-08-30 00:36:15 +0200

    Complete exercise n°6 as per instructions

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+	let stringAll = '';
+	if (times < 0) {
+		return 'ERROR';
+	} else {
+		for (let i = 0; i < times; i++) {
+			stringAll += string;
+		}
+	}
+	return stringAll;
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
