478fab2c7076a6634ef7ec13094accd2a855ad86
Akrame-Amine Dridi <aadridi@gmail.com>
2025-08-30 14:19:52 +0200

    Complete exercise n°9 as per instructions

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const isPositiveInteger = (number) => {
+	return Number.isInteger(number) && number > 0;
+};
 
+const sumAll = function (start, end) {
+	let sum = 0;
+	let a = start < end ? start : end;
+	let b = end > start ? end : start;
+	if (!isPositiveInteger(start) || !isPositiveInteger(end)) {
+		return 'ERROR';
+	} else {
+		for (let i = a; i <= b; i++) {
+			sum += i;
+		}
+	}
+	return sum;
 };
 
 // Do not edit below this line
