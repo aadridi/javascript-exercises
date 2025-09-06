5bb2c55e6f1edafe91457124fc872b2cb4b2eb83
Akrame-Amine Dridi <aadridi@gmail.com>
2025-09-06 20:11:14 +0200

    Complete exercise n°13 as per instructions

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const cleanSentence = (sentence) => {
+	return sentence
+		.split('')
+		.filter((ch) => {
+			let lower = ch.toLowerCase();
+			return (lower >= 'a' && lower <= 'z') || (lower >= '0' && lower <= '9');
+		})
+		.join('')
+		.toLowerCase();
 };
+
+const palindromes = function (sentence) {
+	sentence = cleanSentence(sentence);
+	for (let i = 0; i < sentence.length / 2; i++) {
+		if (sentence.at(i) !== sentence.at(-i - 1)) {
+			return false;
+		}
+	}
+	return true;
+};
+
+console.log(palindromes('racecar')); // true
+console.log(palindromes('tacos')); // false
 
 // Do not edit below this line
 module.exports = palindromes;
