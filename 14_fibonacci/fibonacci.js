e0b6873c02bf7bb2fc2dd4c61694b317e45dce73
Akrame-Amine Dridi <aadridi@gmail.com>
2025-09-07 16:39:52 +0200

    Complete the exercise n°14 as per instructions

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacciMini = function (number) {
+	if (number === 0) {
+		return 0;
+	} else if (number === 1) {
+		return 1;
+	} else {
+		return fibonacciMini(number - 1) + fibonacciMini(number - 2);
+	}
+};
 
+// const fibonacci = function(number) {
+//   let arr = [];
+//   for (let i = 0; i < number; i++) {
+//     arr.push(fibonacciMini(i));
+//   }
+//   return arr[number - 1];
+// };
+
+const fibonacci = function (number) {
+	if (parseInt(number) < 0) {
+		return 'OOPS';
+	} else if (parseInt(number) === 0) {
+		return 0;
+	} else {
+		return fibonacciMini(parseInt(number));
+	}
 };
 
 // Do not edit below this line
