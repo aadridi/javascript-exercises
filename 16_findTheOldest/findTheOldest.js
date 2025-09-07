c53dad8be6fd69c1e8ca7bdaf638178a605da8d8
Akrame-Amine Dridi <aadridi@gmail.com>
2025-09-07 17:40:45 +0200

    Complete exercise n°16 as per instructions

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+	let arraySorted = array.sort((a, b) => {
+		const currentYear = new Date().getFullYear();
+		const ageA = (a.yearOfDeath === undefined ? currentYear : a.yearOfDeath) - a.yearOfBirth;
+		const ageB = (b.yearOfDeath === undefined ? currentYear : b.yearOfDeath) - b.yearOfBirth;
+		return ageA - ageB;
+	});
+	return arraySorted.at(-1);
 };
 
 // Do not edit below this line
